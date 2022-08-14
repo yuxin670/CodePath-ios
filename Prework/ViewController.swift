49d4591aa10b9ed84dddd99b58b40265235f23c7
yuxin670 <107701554+yuxin670@users.noreply.github.com>
2022-08-14 14:52:42 -0400

    Added percent slider and split features

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -15,26 +15,63 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var totalLabel: UILabel!
     
-
+    @IBOutlet weak var percentLabel: UILabel!
+    @IBOutlet weak var percentControl: UISlider!
+    
+    
+    @IBOutlet weak var partySize: UILabel!
+    @IBOutlet weak var partyControl: UIStepper!
+    @IBOutlet weak var personTotalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-    @IBAction func calculateTip(_ sender: Any) {
+    
+    
+    
+    //Caclulate Tip and Total
+    @IBAction func calculateTipFromSlider(_ sender: Any) {
         // Get bill amount from text field input
         let bill = Double(billAmountTextField.text!) ?? 0
         
-        // Get Total ip by multiplying *tipPercentage
-        let tipPercentages = [0.15, 0.18, 0.2]
-        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        // Get tip percentage from slider
+        let sliderSelect = percentControl.value
+        let tipPercentages = sliderSelect * 30
+        
+        //Update Tip Percentage label
+        percentLabel.text = String(format: "%.0f %%", tipPercentages)
+
+        
+        //calcualte tip and total
+        let tip = bill * Double(Int(tipPercentages)) / 100
         let total = bill + tip
         
         // Update Tip Amount Label
         tipAmountLabel.text = String(format: "$%.2f", tip)
         // Update Total Amount
         totalLabel.text = String(format: "$%.2f", total)
+ 
+        //Get Party Size
+        partySize.text = String(Int( partyControl.value))
+        
+        //Splitting Total
+        let personTotal =
+        total / Double(Int( partyControl.value))
+        
+        personTotalLabel.text = String(format: "$%.2f", personTotal)
+        
+        
     }
     
+    // Splitting Total
+    @IBAction func partyStepper(_ sender: Any) {
+        
+        //Get Party Size
+        partySize.text = String(Int( partyControl.value))
+        
+    }
 }
 
+
+    
